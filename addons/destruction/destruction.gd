18ab89dd6f448c00b7beb275e12e6346f10bd315
Jummit <jummit@web.de>
2020-09-05 10:12:03 +0200

    refactor and configuration warnings

--- addons/destruction/destruction.gd
+++ addons/destruction/destruction.gd
@@ -1,19 +1,44 @@
+tool
 extends Node
 
-onready var DestructionUtils := preload("res://addons/destruction/DestructionUtils.gd")
+"""
+Handles destruction of the parent node
+
+When `destroy` is called, the parent node gets removed
+and shards are added to the `shard_container`. A `shard_template` is used
+to configure how the `shard_scene` will be converted to `RigidBodies`.
+"""
+
 export var shard_template : PackedScene = preload("res://addons/destruction/ShardTemplates/DefaultShardTemplate.tscn")
 export var shard_scene : PackedScene
-export var shard_container : NodePath
+export var shard_container := @"../../" setget set_shard_container
 
-onready var to_add_shards_to := get_parent().get_parent()
-
-func _ready():
-	if shard_container:
-		to_add_shards_to = get_node(shard_container)
-
+const DestructionUtils = preload("res://addons/destruction/DestructionUtils.gd")
 
 func destroy() -> void:
 	var shards := DestructionUtils.create_shards(shard_scene.instance(), shard_template)
-	to_add_shards_to.add_child(shards)
+	get_node(shard_container).add_child(shards)
 	shards.global_transform.origin = get_parent().global_transform.origin
 	get_parent().queue_free()
+
+
+func set_shard_container(to : NodePath) -> void:
+	shard_container = to
+	update_configuration_warning()
+
+
+func _notification(what : int) -> void:
+	if what == NOTIFICATION_PATH_CHANGED:
+		update_configuration_warning()
+
+
+func _get_configuration_warning() -> String:
+	return "The shard container is a PhysicsBody or has a PhysicsBody as a parent. This will make the shards added to it behave in unexpected ways." if get_node(shard_container) is PhysicsBody or _has_parent_of_type(get_node(shard_container), PhysicsBody) else ""
+
+
+static func _has_parent_of_type(node : Node, type) -> bool:
+	if not node.get_parent():
+		return false
+	if node.get_parent() is type:
+		return true
+	return _has_parent_of_type(node.get_parent(), type)
