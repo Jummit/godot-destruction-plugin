4b16667ab514367a9023e6448be67d9c45235dff
zachar3 <handyboyservices1@gmail.com>
2023-04-11 17:32:51 +0530

    Simplified impulse by changing randf_range() to randf() as it was unneeded; and scaled down velocity to reduce explosion size to be more similar to in Godot 3.x

--- addons/destruction/ShardTemplates/ExplosionShardTemplate.gd
+++ addons/destruction/ShardTemplates/ExplosionShardTemplate.gd
@@ -3,6 +3,6 @@ extends RigidBody3D
 
 func _ready():
 	await(get_tree().create_timer(0.0000001).timeout) #Required to skip the first loop due to bug #29888
-	apply_impulse(Vector3(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1)), -position.normalized() * 50)
+	apply_impulse(Vector3(randf(), randf(), randf()), -position.normalized() * 10)
 	
 	
