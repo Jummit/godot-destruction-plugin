3810b3d40b6d188994960c8efe2d48530edba71e
zachar3 <handyboyservices1@gmail.com>
2023-04-12 10:20:31 +0530

    Better workaround for first frame apply_impulse bug

--- addons/destruction/ShardTemplates/ExplosionShardTemplate.gd
+++ addons/destruction/ShardTemplates/ExplosionShardTemplate.gd
@@ -2,7 +2,8 @@ extends RigidBody3D
 
 
 func _ready():
-	await(get_tree().create_timer(0.0000001).timeout) #Required to skip the first loop due to bug #29888
+	await(get_tree().physics_frame) #Both of these are required to skip the first loop due to bug https://github.com/godotengine/godot/issues/75934
+	await(get_tree().physics_frame)
 	apply_impulse(Vector3(randf(), randf(), randf()), -position.normalized() * 10)
 	
 	
