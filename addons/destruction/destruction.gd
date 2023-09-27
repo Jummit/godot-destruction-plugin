23cb32332a1515fe9efe0983c1aacefd236be2e2
Jummit <jummit@web.de>
2023-09-27 18:05:03 +0200

    Remove threading for now

--- addons/destruction/destruction.gd
+++ addons/destruction/destruction.gd
@@ -5,22 +5,24 @@ extends Node
 
 ## Handles destruction of the parent node.
 ##
-## When [method destroy] is called, the parent node gets removed
-## and shards are added to the [member shard_container]. [member shard] is
-## instantiated to configure how the [member shard] will be converted to
-## [RigidBody3D]s.
+## When [method destroy] is called, the parent node is freed and
+## shards are added to the [member shard_container]. The [member shard]
+## scene is used as a template for [RigidBody3D]s created from the meshes
+## inside the [member fragmented] scene..
 
-## A scene of the fragmented mesh containing multiple [MeshInstance]s.
+## A scene of the fragmented mesh containing multiple [MeshInstance3D]s.
 @export var fragmented: PackedScene: set = set_fragmented
 ## The shard which is instanced for every part of the fragmented version.
 @export var shard := preload("shard.tscn"): set = set_shard
 ## The node where created shards are added to.
-@onready @export var shard_container: Node = get_node("../../")
+@onready @export var shard_container := get_node("../../")
+
 @export_group("Animation")
 ## How many seconds until the shards fade away. Set to -1 to disable fading.
 @export var fade_delay := 2.0
 ## How many seconds until the shards shrink. Set to -1 to disable shrinking.
 @export var shrink_delay := 2.0
+
 @export_group("Collision")
 ## The [member RigidBody3D.collision_layer] set on the created shards.
 @export_flags_3d_physics var collision_layer = 1
@@ -32,18 +34,33 @@ static var cached_meshes := {}
 ## Cached collision shapes.
 static var cached_shapes := {}
 
-signal _shards_created(shards)
-
 ## Remove the parent node and add shards to the shard container.
 func destroy(explosion_power := 1.0) -> void:
-	var thread := Thread.new()
-	thread.start(_create_shards.bind(explosion_power))
-	var shards = await _shards_created
-	thread.wait_to_finish()
+	var shards = _create_shards(explosion_power)
 	shard_container.add_child(shards)
 	shards.global_transform.origin = get_parent().global_transform.origin
-	shards.top_level = true
 	get_parent().queue_free()
+
+
+func set_shard(to: PackedScene) -> void:
+	shard = to
+	if is_inside_tree():
+		get_tree().node_configuration_warning_changed.emit(self)
+
+
+func set_fragmented(to: PackedScene) -> void:
+	fragmented = to
+	if is_inside_tree():
+		get_tree().node_configuration_warning_changed.emit(self)
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings := []
+	if not fragmented:
+		warnings.append("No fragmented version set")
+	elif not shard:
+		warnings.append("No shard template set")
+	return warnings
 
 
 func _create_shards(explosion_power : float):
@@ -74,25 +91,4 @@ func _create_shards(explosion_power : float):
 		new_shard.shrink_delay = shrink_delay
 		
 		shards.add_child(new_shard)
-	emit_signal.call_deferred("_shards_created", shards)
-
-
-func set_shard(to: PackedScene) -> void:
-	shard = to
-	if is_inside_tree():
-		get_tree().node_configuration_warning_changed.emit(self)
-
-
-func set_fragmented(to: PackedScene) -> void:
-	fragmented = to
-	if is_inside_tree():
-		get_tree().node_configuration_warning_changed.emit(self)
-
-
-func _get_configuration_warnings() -> PackedStringArray:
-	var warnings := []
-	if not fragmented:
-		warnings.append("No fragmented version set")
-	elif not shard:
-		warnings.append("No shard template set")
-	return warnings
+	return shards
